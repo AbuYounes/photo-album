061157e86e031ad5505e88479b53a6f3f96d59ee
Farid Benhaimoud <farid.benhaimoud@gmail.com>
2022-05-14 18:42:00 +0200

    Moved to view Folder

--- app/src/main/java/nl/farid/android/photoalbum/presentation/view/MainActivity.kt
+++ app/src/main/java/nl/farid/android/photoalbum/presentation/view/MainActivity.kt
@@ -1,8 +1,11 @@
-package nl.farid.android.photoalbum
+package nl.farid.android.photoalbum.presentation.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import nl.farid.android.photoalbum.R
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
