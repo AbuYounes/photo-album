c74b17da161b4efc1b151dd151d2903434ef6fde
Farid Benhaimoud <farid.benhaimoud@gmail.com>
2022-05-15 15:29:44 +0200

    Added Use cases and implemented logic

--- app/src/main/java/nl/farid/android/photoalbum/business/usecase/abstraction/IGetAlbums.kt
+++ app/src/main/java/nl/farid/android/photoalbum/business/usecase/abstraction/IGetAlbums.kt
@@ -1,4 +1,12 @@
 package nl.farid.android.photoalbum.business.usecase.abstraction
 
+import nl.farid.android.photoalbum.model.app_model.Album
+
 interface IGetAlbums {
+    suspend fun getAlbums(): GetAlbumResult
+}
+
+sealed class GetAlbumResult{
+    data class Success(val list: List<Album>): GetAlbumResult()
+    data class Error(val e: Exception): GetAlbumResult()
 }
