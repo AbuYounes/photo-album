576a6b34ff1a43926eeeeb4378f9cf2cce574b8f
Farid Benhaimoud <farid.benhaimoud@gmail.com>
2022-05-15 15:28:26 +0200

    Added functions to PhotoAlbumCacheDataSource.kt and PhotoAlbumNetworkDataSource.kt

--- app/src/main/java/nl/farid/android/photoalbum/business/data/network/abstraction/IPhotoAlbumNetworkDataSource.kt
+++ app/src/main/java/nl/farid/android/photoalbum/business/data/network/abstraction/IPhotoAlbumNetworkDataSource.kt
@@ -1,4 +1,9 @@
 package nl.farid.android.photoalbum.business.data.network.abstraction
 
+import nl.farid.android.photoalbum.model.dto.AlbumDTO
+import nl.farid.android.photoalbum.model.dto.PhotoDTO
+
 interface IPhotoAlbumNetworkDataSource {
+    suspend fun getAlbums(): List<AlbumDTO>
+    suspend fun getPhotosFromAlbum(albumId: Int): List<PhotoDTO>
 }
--- app/src/main/java/nl/farid/android/photoalbum/business/data/network/implementation/PhotoAlbumNetworkDataSource.kt
+++ app/src/main/java/nl/farid/android/photoalbum/business/data/network/implementation/PhotoAlbumNetworkDataSource.kt
@@ -1,6 +1,8 @@
 package nl.farid.android.photoalbum.business.data.network.implementation
 
 import nl.farid.android.photoalbum.business.data.network.abstraction.IPhotoAlbumNetworkDataSource
+import nl.farid.android.photoalbum.model.dto.AlbumDTO
+import nl.farid.android.photoalbum.model.dto.PhotoDTO
 import nl.farid.android.photoalbum.presentation.datasource.network.service.PhotoAlbumService
 import javax.inject.Inject
 
@@ -8,4 +10,12 @@ class PhotoAlbumNetworkDataSource
 @Inject constructor(
     private val photoAlbumService: PhotoAlbumService
 ): IPhotoAlbumNetworkDataSource{
+
+    override suspend fun getAlbums(): List<AlbumDTO> =
+        photoAlbumService.getAlbums()
+
+
+    override suspend fun getPhotosFromAlbum(albumId: Int): List<PhotoDTO> =
+        photoAlbumService.getPhotosFromAlbum(albumId)
+
 }
